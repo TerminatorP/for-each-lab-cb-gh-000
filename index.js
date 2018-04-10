24599a093c9c7b9ecc9a8912345d6c0e6dc6d02e
Prabesh Subedi <subediprabesh1@gmail.com>
2018-04-10 08:06:37 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,9 @@
+function iterativeLog(array) {
+  array.forEach((el,i) => console.log(`${index}: ${element}`));
+}
+
+function iterate(callback) {
+  let arrFriends = ['Kenny', 'Nebi'];
+  arrFriends.forEach(callback);
+  return arrFriends;
+}
